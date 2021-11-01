297d6bb43f301a1c5cfcba4805d35d476a412774
root <root@rpi.lan>
2021-11-01 22:00:16 +0000

    first commit

---
+++ disc_bot.py
@@ -0,0 +1,26 @@
+import discord
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('Hello bot'):
+        await message.channel.send('Hello human trash!')
+
+    if message.content.startswith('Hello'):
+        await message.channel.send('Hello human trash!')
+
+    if message.content.startswith('Trash'):
+        await message.channel.send('NO U')
+
+client.run('OTA0NDc3NTgzMDUwMTQ5OTIw.YX8GZA.ZWpHR0BxYfdGD4KNLa4nEGnGKas')
